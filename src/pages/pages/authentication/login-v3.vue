094e46ec20c5265ec94fc2bcd43fa513b52538f6
Your Name <you@example.com>
2024-05-28 13:43:07 +0300

    --init

---
+++ src/pages/pages/authentication/login-v3.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <VCard title="Create Awesome 🙌">
+      <VCardText>This is your second page dddddddddddddafvA  pppages.</VCardText>
+      <VCardText>
+        \
+
+        <form>
+          <label for="uname"><b>Email</b></label>
+          <input
+           
+            type="email"
+            placeholder="Enter Email"
+            required
+          >
+          <br><br>
+          <label for="psw"><b>Password</b></label>
+          <input
+         
+            type="password"
+            placeholder="Enter Password"
+            required
+          >
+          <br><br>
+          <button type="submit">
+            Login
+          </button>
+        </form>
+    
+        Chocolate sesame snaps pie carrot cake pastry pie lollipop muffin.
+        Carrot cake dragée chupa chups jujubes. Macaroon liquorice cookie
+        wafer tart marzipan bonbon. Gingerbread jelly-o dragée
+        chocolate.
+      </VCardText>
+    </VCard>
+  </div>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,48 @@
+<template>
+  <form
+    method="post"
+    @submit="Submit"
+  >
+    <label for="uname"><b>Email</b></label>
+    <input
+      v-model="form.email"
+      type="email"
+      placeholder="Enter Email"
+      required
+    >
+    <br><br>
+    <label for="psw"><b>Password</b></label>
+    <input
+      v-model="form.password"
+      type="password"
+      placeholder="Enter Password"
+      required
+    >
+    <br><br>
+    <button type="submit">
+      Login
+    </button>
+  </form>
+  <br>
+  {{ $store.state.email }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+    }
+  },
+  methods: {
+    Submit(event){
+      event.preventDefault()
+      this.$store.commit('login', this.form.email)
+    },
+  },
+    
+}
+</script>
